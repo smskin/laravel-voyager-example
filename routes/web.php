59473eb88cd4e44f6eb31cfdad9ab12f819265bb
SMSkin <serg@msoinvest.com>
2020-04-15 18:07:11 +0300

    remove voyager routes from web router (it's used in admin router)

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
